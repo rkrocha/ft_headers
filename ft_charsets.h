2bc122a997fb3c6d2a13b6ad4da24549bc7fd5e0
rkrocha <rafaelkr7@gmail.com>
2021-11-16 15:24:20 -0300

    added ft_charsets.h

---
+++ ft_charsets.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_charsets.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/16 07:51:00 by rkochhan          #+#    #+#             */
+/*   Updated: 2021/11/16 15:23:21 by rkochhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_CHARSETS_H
+# define FT_CHARSETS_H
+
+# define BIN_BASE		"01"
+# define OCTAL_BASE		"01234567"
+# define DECIMAL_BASE	"0123456789"
+# define LOWER_HEX_BASE	"0123456789abcdef"
+# define UPPER_HEX_BASE	"0123456789ABCDEF"
+# define BLANK_SPACES	" \f\n\r\t\v"
+
+#endif
